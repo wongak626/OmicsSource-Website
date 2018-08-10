51ed4ae9b72b10f0aaee33eaaf3156efeb4a52af
Alexander Wong <wongak626@gmail.com>
2018-08-10 01:35:12 -0400

    change port number

--- app.js
+++ app.js
@@ -51,10 +51,10 @@ app.use("/",indexRoutes);
 app.use("/tools",toolRoutes);
 app.use("/tools/:id/comments",commentRoutes);
 
-// app.listen(process.env.port || 4000, function(){
-// 	console.log("YelpCamp Server Has Started!");
-// });
-
-app.listen(process.env.port, process.env.IP, function(){
-	console.log("OmicsSource Server Has Started!");
+app.listen(process.env.port || 3000, function(){
+	console.log("YelpCamp Server Has Started!");
 });
+
+// app.listen(process.env.port, process.env.IP, function(){
+// 	console.log("OmicsSource Server Has Started!");
+// });
